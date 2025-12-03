00e74f72845a0bf4a4bc06faa29190ee6ef18b19
Oscar Katz <ogkatz@googlemail.com>
2025-12-03 19:50:25 +0000

    Run black

--- day3.py
+++ day3.py
@@ -4,6 +4,7 @@ import typing as t
 
 from itertools import pairwise
 
+
 def _yield_rows():
     with open("day3_inp.txt", "r") as f:
         for line in f.readlines():
@@ -12,26 +13,31 @@ def _yield_rows():
 
             yield [int(elem) for elem in ret]
 
+
 def _get_val(introw: t.Sequence[int]):
     tot = 0
     for idx, val in enumerate(reversed(introw)):
-        tot += val * (10 ** idx)
+        tot += val * (10**idx)
 
     return tot
+
 
 def get_largest_val(introw: t.Sequence[int], idx: int = 12):
     current_arr, remaining_arr = introw[:idx], introw[idx:]
 
     for num in remaining_arr:
-        for idx, (first_num, second_num) in enumerate(pairwise([*current_arr, num])):
+        for idx, (first_num, second_num) in enumerate(
+            pairwise(chain(current_arr, [num]))
+        ):
             if second_num >= first_num:
-                proposed = list(chain(current_arr[:idx], current_arr[idx+1:], [num]))
-                
+                proposed = list(chain(current_arr[:idx], current_arr[idx + 1 :], [num]))
+
                 if _get_val(proposed) > _get_val(current_arr):
                     current_arr = proposed
 
                     break
-    
+
     return _get_val(current_arr)
+
 
 print(sum(get_largest_val(introw) for introw in _yield_rows()))
