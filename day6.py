9e7101aff9eecb9e66999aea766837eac375b450
Oscar Katz <ogkatz@googlemail.com>
2025-12-06 12:46:22 +0000

    Run black and better malformed doc handling

--- day6.py
+++ day6.py
@@ -1,26 +1,30 @@
 from itertools import zip_longest
 import math
-import re
+
 
 def process_ret(ret: list[tuple[str, ...]]):
     num_tups: list[list[str]] = []
     ops: set[str] = set()
-    
+
     for tup in ret:
         *nums, op = tup
 
         num_tups.append(nums)
         if op in ("*", "+"):
             ops.add(op)
-    
+
     if len(ops) == 1:
         final_op = next(iter(ops))
         yield final_op, num_tups
 
+    else:
+        raise ValueError(f"Malformed ret, had more than one op. {ret=}")
+
+
 def get_question():
     with open("day6_inp.txt", "r") as f:
-        rows = [list(line.strip("\n")) for line in f.readlines()] 
-    
+        rows = [list(line.strip("\n")) for line in f.readlines()]
+
     tposed = tuple(zip_longest(*rows, fillvalue=" "))
 
     line_len = len(tposed[0])
@@ -32,14 +36,15 @@ def get_question():
     for tup in tposed:
         if tup == split_tup:
             yield from process_ret(ret)
-            
+
             ret = []
-        
+
         else:
             ret.append(tup)
-    
+
     yield from process_ret(ret)
-    
+
+
 def process_question():
     for operation, num_tups in get_question():
         nums = [int("".join(num_tup).strip()) for num_tup in num_tups]
@@ -49,5 +54,6 @@ def process_question():
                 yield sum(nums)
             case "*":
                 yield math.prod(nums)
+
 
 print(sum(process_question()))
